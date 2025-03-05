dbfb904e484e312936ee39ae0c34892137a9f1d6
nada yahya <56319084+nada969@users.noreply.github.com>
2025-03-05 03:05:41 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,33 +1,28 @@
-
-# from .models import Author , Book , Librarian , Library
-
-
-# ####Query all books by a specific author.
-
-# all_books = Book.objects.order_by('author')
-
-# # List all books in a library.
-# books = Library.objects.get(name=library_name)
-# books.all()
-
-# # Retrieve the librarian for a library.
-# all_librarian = Librarian.objects.all()
-
 from .models import Author , Book , Librarian , Library
 
-
 ####Query all books by a specific author.
-author = Author.objects.create('name')
-book1 = Book.objects.create('book1','name')
-book1 = Book.objects.create('book2','name')
-all_books = author.books.all()
+def specific_author(author_name):
+    author = Author.objects.get(name=author_name)
+    all_books = Book.objects.filter(author=author)
+    
+    return all_books
+
+##### ex:
+# author = Author.objects.create('name')
+# book1 = Book.objects.create('book1','name')
+# book1 = Book.objects.create('book2','name')
+# all_books = author.books.all()
 
 
-# List all books in a library.
-# List all books in a library.
-books = Library.objects.get(name=library_name)
-books.all()
 
-# Retrieve the librarian for a library.
+# # List all books in a library.
+def list_books(library_name):
+    library = Library.objects.get(name = library_name)
+    books = library.books.all()
+    return books
 
-all_librarian = Librarian.objects.all()
+
+# # Retrieve the librarian for a library.
+def all_librarian(library_name):
+    all_librarian = Librarian.objects.get(Library = library_name)
+    return all_librarian
