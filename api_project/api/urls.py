604d76fa03e56e32fb2420c910237e1a74267bd4
nada yahya <56319084+nada969@users.noreply.github.com>
2025-03-08 03:42:40 +0200

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.urls import path , include
 from rest_framework.routers import DefaultRouter 
 
-router.register(r'books_all', BookViewSet, basename='book_all')
+router.register(r'books_all', BookViewSet.DefaultRouter(), basename='book_all')
 #######3️⃣ Register endpoints in urls.py:
 urlpatterns = [
     path('books/',views.BookList.as_view(),name='BookList'),    # Maps to the BookList view
