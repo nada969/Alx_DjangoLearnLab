2fd4e13f2f0e667e8839f64217effb29f9dfca89
nada969 <nadamousa969@gmail.com>
2025-03-23 23:46:49 +0200

    add

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 # class User(models.Model):
 #     user_id = models.IntegerField()
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content= models.TextField()
     published_date= models.DateTimeField(auto_now_add=True)
     author= models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')      ### to Django’s User model, with a relation to handle multiple posts by a single author.
+    tags = models.ManyToManyField(Tag,related_name='posts')
 
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-    posts = models.ManyToManyField(Post,related_name='tag')
 
